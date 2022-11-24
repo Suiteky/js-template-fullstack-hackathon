368fb1b812aafcf3a1cb07ca25ac2d431d8de77b
Julien GRELLIER <grllr.julian.ptrs@gmail.com>
2022-11-24 21:00:51 +0100

    home left hendleclick

--- frontend/src/components/Image.jsx
+++ frontend/src/components/Image.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 
 export default function Image({ image }) {
+  const handleClick = (e) => {
+    console.warn("j'existe");
+  };
+
   return (
     <div>
-      <img src={image.url} alt="img" />
+      <img
+        src={image.src.large}
+        alt={image.alt}
+        className="landscape"
+        onClick={handleClick}
+      />
     </div>
   );
 }
