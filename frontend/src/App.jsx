1fcc75a3ab4cb5be80f951ff457b618137eb83d3
lana duval <lanaduval09@gmail.com>
2022-11-24 22:23:54 +0100

    add router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,16 @@
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import { ImagesContextProvider } from "./contexts/ImagesContext";
 
 function App() {
   return (
-    <ImagesContextProvider>
-      <Home />
-    </ImagesContextProvider>
+    <BrowserRouter>
+      <ImagesContextProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </ImagesContextProvider>
+    </BrowserRouter>
   );
 }
 
